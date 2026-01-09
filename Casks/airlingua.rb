39325a050815e27fd52119161d9cc2735a78d08e
fuwasegu <admin@fuwasegu.com>
2026-01-09 13:40:49 +0900

    Add AirLingua cask

---
+++ Casks/airlingua.rb
@@ -0,0 +1,27 @@
+cask "airlingua" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/fuwasegu/AirLingua/releases/download/v#{version}/AirLingua-#{version}.zip"
+  name "AirLingua"
+  desc "Local LLM translation app for macOS"
+  homepage "https://github.com/fuwasegu/AirLingua"
+
+  depends_on macos: ">= :sonoma"
+
+  app "AirLingua.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/AirLingua.app"],
+                   sudo: false
+    system_command "/usr/bin/SetFile",
+                   args: ["-a", "E", "#{appdir}/AirLingua.app"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/AirLingua",
+    "~/Library/Preferences/com.personal.LocalTranslate.plist",
+  ]
+end
